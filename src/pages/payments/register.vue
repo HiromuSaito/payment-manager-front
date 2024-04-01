694b35657e4be01ae97090d8c67ab6d3c9f29717
HiromuSaito <118810089+HiromuSaito@users.noreply.github.com>
2024-04-02 00:21:35 +0900

    #3:払出日をdatepickerに修正 (#7)

--- src/pages/payments/register.vue
+++ src/pages/payments/register.vue
@@ -7,23 +7,49 @@
       </v-row>
       <v-row>
         <v-form ref="form" lazy-validation class="w-400">
-          <v-text-field v-model="paymentDate" label="払出日" required></v-text-field>
-
-          <v-select v-model="itemCode" :items="itemCodes" label="商品コード" required />
-          <v-select
-            v-model="paymentType"
-            :items="items"
-            item-title="value"
-            item-value="key"
-            label="払出種別"
-            required
-          ></v-select>
-
-          <v-text-field v-model="paymentAmount" label="数量" required></v-text-field>
-          <v-textarea label="備考" v-model="note"></v-textarea>
-
-          <v-btn color="error" class="mr-4" @click.prevent="cancel"> キャンセル</v-btn>
-          <v-btn @click.prevent="register" color="success" class="mr-4"> 登録 </v-btn>
+          <v-row>
+            <v-menu ref="menu" :close-on-content-click="false">
+              <template v-slot:activator="{ props }">
+                <v-text-field
+                  v-bind="props"
+                  label="払出日"
+                  required
+                  prepend-icon="mdi-calendar"
+                  v-model="paymentFormattedDate"
+                />
+              </template>
+              <v-date-picker
+                color="primary"
+                v-model="paymentDate"
+                hide-header
+                @update:modelValue="formatDate()"
+              >
+              </v-date-picker>
+            </v-menu>
+          </v-row>
+          <v-row>
+            <v-select v-model="itemCode" :items="itemCodes" label="商品コード" required />
+          </v-row>
+          <v-row>
+            <v-select
+              v-model="paymentType"
+              :items="items"
+              item-title="value"
+              item-value="key"
+              label="払出種別"
+              required
+            ></v-select>
+          </v-row>
+          <v-row>
+            <v-text-field v-model="paymentAmount" label="数量" required></v-text-field>
+          </v-row>
+          <v-row>
+            <v-textarea label="備考" v-model="note"></v-textarea>
+          </v-row>
+          <v-row>
+            <v-btn color="error" class="mr-4" @click.prevent="cancel"> キャンセル</v-btn>
+            <v-btn @click.prevent="register" color="success" class="mr-4"> 登録 </v-btn>
+          </v-row>
         </v-form>
       </v-row>
     </v-col>
@@ -37,7 +63,8 @@ import { PAYMENT_TYPE } from '../../const';
 import payment from '../../api/payment';
 import itemsApi from '../../api/items';
 
-const paymentDate = ref('');
+const paymentDate = ref();
+const paymentFormattedDate = ref();
 const paymentAmount = ref(0);
 const itemCode = ref('');
 const paymentType = ref<'PLANED_PAYMENT' | 'NON_PLANED_PAYMENT'>('PLANED_PAYMENT');
@@ -46,6 +73,10 @@ const items = Object.entries(PAYMENT_TYPE).map(([key, value]) => ({
   key,
   value,
 }));
+
+const formatDate = () => {
+  paymentFormattedDate.value = `${paymentDate.value.getFullYear()}/${paymentDate.value.getMonth().toString().padStart(2, '0')}/${paymentDate.value.getDate().toString().padStart(2, '0')}`;
+};
 
 const itemCodes = ref([]);
 itemsApi.getCodeList().then((res) => {
@@ -59,7 +90,7 @@ const cancel = () => {
 const register = async () => {
   const req = {
     paymentType: paymentType.value,
-    paymentDate: paymentDate.value,
+    paymentDate: paymentFormattedDate.value,
     itemCode: itemCode.value,
     paymentAmount: paymentAmount.value,
     note: note.value,
